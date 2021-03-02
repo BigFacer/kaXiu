cbd4f608f7ed638836cf51072e0060e34a33f7a7
HeroAhao <184837552@qq.com>
2021-03-02 17:31:25 +0800

    20210302

---
+++ miniprogram/pages/component/components.js
@@ -0,0 +1,25 @@
+let userFun = {};
+userFun.sevenDate = function(){
+  let newDate = new Date()
+  let nowTime = newDate.getTime();
+  let oneDayTime = 24*60*60*1000;
+  let selectDayList = [] 
+  for(let i = 0; i< 10; i++){
+    if(selectDayList.length > 7){
+      return selectDayList
+    }else{
+      let showTime = nowTime+ i*oneDayTime
+      let myDate = new Date(showTime);
+      let year = myDate.getFullYear();
+      let month = myDate.getMonth()+1;
+      let day = myDate.getDate();
+      let time = year + '-' +  month+ '-' + day
+      let str = "星期" + "日一二三四五六".charAt(myDate.getDay())
+      if(str != '星期一'){
+        selectDayList.push(time+ str)
+      }
+   
+    }
+  }
+  return selectDayList;
+}
--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -1,5 +1,27 @@
+let userFun = require('../component/components')
 Page({
   data: {
-    
+    startDate:'',
+    endDate:'',
+    index: 0,
+    date: '',
+    newDate: new Date().getFullYear()+ '-'+(new Date().getMonth()+1)+'-'+new Date().getDate()
+  },
+  onLoad: function(){
+  //  let a = userFun.sevenDate();
+   console.log(userFun)
+   
+     this.setData({
+       date:  this.data.newDate,
+       startDate: this.data.newDate,
+       endDate: '2021-03-09'
+       
+     })  
+  },
+  bindDateChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
   }
 })
