56d4376d847bf3a3df878846d9bcbdcefa68ac23
HeroAhao <184837552@qq.com>
2021-03-15 18:09:34 +0800

    2021031502

--- cloudfunctions/getWDay/index.js
+++ cloudfunctions/getWDay/index.js
@@ -1,13 +1,26 @@
 const clude = require('wx-server-sdk');
-const rq = require('request-promise');
+const rp = require('request-promise');
 clude.init();
 exports.main = async (event, context) => {
-  let url = 'http://v3.wufazhuce.com:8000/api/channel/one/0/上海';
-  return await rq(url).then((res)=> {
-    console.log("成功"+res)
-    return res
-  }).catch((err) => {
-    console.log('失败')
-    return '失败'
-  })
+  
+  const get_options = {
+
+    method: 'GET',
+    url:'http://v3.wufazhuce.com:8000/api/channel/one/0/shanghai',
+    qs: {},
+
+    json: true
+
+   
+
+  };
+  const get_res = await rp(get_options);
+  return {get_res}
+  // return await rq(url).then((res)=> {
+  //   console.log("成功"+res)
+  //   return res
+  // }).catch((err) => {
+  //   console.log('失败')
+  //   return '失败'
+  // })
 }
